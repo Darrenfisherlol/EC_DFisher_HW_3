2dd1b1aa2091b96aa80b5d6046d320138e4e83d3
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-09-12 14:23:42 -0500

    Add files via upload

---
+++ showData.php
@@ -0,0 +1,42 @@
+
+<?php require_once("indexHeader.php")?>
+
+
+
+    <?php
+        $servername = "localhost";
+        $username = "darrenfi_homework3";
+        $password = "MISHomework3";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT ClassId, ClassName FROM Class";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["ClassId"]. " - Name: " . $row["ClassName"]. "<br>";
+        }
+    } 
+    else {
+        echo "0 results";
+    }
+
+    $conn->close();
+    ?>
+
+

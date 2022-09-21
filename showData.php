1d87d4f59f9e44972d4b847e99f2f067a427f770
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-09-21 15:36:54 -0500

    Add files via upload

--- showData.php
+++ showData.php
@@ -23,6 +23,75 @@
         <br>
 
 
+
+<!-- 2nd middle part of page -->
+<div id="middleDatashow">
+            
+            The building you wanted is: <?php echo $_POST["BuildingName"]; ?><br>
+
+            <?php
+                $servername = "localhost";
+                $username = "darrenfi_homework3";
+                $password = "MISHomework3";
+                $dbname = "darrenfi_homework_3";
+            // Create connection
+            $conn = new mysqli($servername, $username, $password);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+            ?>
+                <tr>
+                    <br>
+                    <td> Class Id <a href="ClassId.php?id="><?=$row["ClassId"]?></a></td>
+                    <br>
+                    <td> Class Name <a href="ClassName.php?id="> <?=$row["ClassName"]?></a> </td>
+                    <br>
+                    <td> Building Id<a href="BuildingId.php?id="> <?=$row["BuildingId"]?></a> </td>
+                    <br>
+                </td>
+            <?php
+                }
+            } 
+            else {
+                echo "0 results";
+            }
+
+            $conn->close();
+            ?>
+
+        </div>
+
+
+        <br>
+        <div>^^ User chooses href to go to another page & sort by it</div>
+        <br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
 
 </body>

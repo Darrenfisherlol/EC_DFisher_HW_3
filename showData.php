715939e24b1761e30dba89925e00992a00f8eea6
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-09-21 16:32:09 -0500

    Add files via upload

--- showData.php
+++ showData.php
@@ -79,77 +79,71 @@
         </div>
 
 
-            <br>
-            <br>
-            <br>
+        <br>
 
 
-            <div id="middleDatashow">
+        <div id="middleDatashow">
 
-                <div id="HeaderForDesc"> 
-                    Printed below is a query of the Class the user entered with class id 
-                </div>
-
-                <?php
-                    
-                    $servername = "localhost";
-                    $username = "darrenfi_homework3";
-                    $password = "MISHomework3";
-                    $dbname = "darrenfi_homework_3";
-                    // Create connection
-                    $conn = new mysqli($servername, $username, $password);
-
-                    // Check connection
-                    if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                    }
-
-                    // Create connection
-                    $conn = new mysqli($servername, $username, $password, $dbname);
-                    // Check connection
-                    if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                    }
-
-                    $Varname = $_POST["ClassId"];  
-
-                    $sql = "SELECT * FROM Class WHERE ClassId = $Varname";
-
-                    $result = $conn->query($sql);
-
-                    if ($result->num_rows > 0) {
-                        // output data of each row
-                        while($row = $result->fetch_assoc()) {
-                    ?>
-                        <tr>
-                            <td> class id: <?=$row["ClassId"]?></td>
-                            <br>
-                            <td> class Name: <?=$row["ClassName"]?></td>
-                            <br>
-                            <td> building Id: <?=$row["BuildingId"]?></td>
-                        </td>
-                    <?php
-                        }
-                    } 
-                    else {
-                        echo "0 results";
-                    }
-
-                    $conn->close();
-                ?>
+            <div id="HeaderForDesc"> 
+                Printed below is a query of the Class the user entered with class id 
             </div>
+
+            <?php
+                
+                $servername = "localhost";
+                $username = "darrenfi_homework3";
+                $password = "MISHomework3";
+                $dbname = "darrenfi_homework_3";
+                // Create connection
+                $conn = new mysqli($servername, $username, $password);
+
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $Varname = $_POST["ClassId"];  
+
+                $sql = "SELECT * FROM Class WHERE ClassId = $Varname";
+
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                ?>
+                    <tr>
+                        <td> class id: <?=$row["ClassId"]?></td>
+                        <br>
+                        <td> class Name: <?=$row["ClassName"]?></td>
+                        <br>
+                        <td> building Id: <?=$row["BuildingId"]?></td>
+                    </td>
+                <?php
+                    }
+                } 
+                else {
+                    echo "0 results";
+                }
+
+                $conn->close();
+            ?>
+        </div>
        
         
-
-            
         <br>
-        <br>
-        <br>
-
+ 
 
         <div id="middleDatashow"> 
 
-            <div>
+            <div id="HeaderForDesc">
                 This will print out all building info
             </div>
 
@@ -204,12 +198,11 @@
 
 
         <br>
-        <br>
-        <br>
+
 
         <div id="middleDatashow">
             
-            <div> 
+            <div id="HeaderForDesc"> 
                 Href print below
             </div>
 

504d8dde7265ac3252e5632be1f98ec92178ca20
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-09-21 15:36:11 -0500

    Add files via upload

--- showData.php
+++ showData.php
@@ -15,53 +15,7 @@
 
         <br>
         
-        <!-- Top that displays the class with the ID entered -->
-        <div id="headerShowDate">
-            <?php
-                $servername = "localhost";
-                $username = "darrenfi_homework3";
-                $password = "MISHomework3";
-                $dbname = "darrenfi_homework_3";
-            // Create connection
-            $conn = new mysqli($servername, $username, $password);
-
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            $sql = "SELECT * FROM Class where ClassId = ".echo $_POST["ClassId"];."";
-
-            echo $sql;
-
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                // output data of each row
-                while($row = $result->fetch_assoc()) {
-            ?>
-                <tr>
-                    <td><?=$row["ClassId"]?></td>
-                    <td><?=$row["ClassName"]?></td>
-                    <td><?=$row["BuildingId"]?></td>
-
-                </td>
-            <?php
-                }
-            } 
-            else {
-                echo "0 results";
-            }
-
-            ?>
-        </div>
+        
 
 
         <br>
